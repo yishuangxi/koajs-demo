b013887e2d814cbf3105f7dfcd55e900564c8a79
yishuangxi <2546618112@qq.com>
2016-10-11 20:19:57 +0800

    add logger

--- mall-koa/front/src/store/createStore.js
+++ mall-koa/front/src/store/createStore.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk'
 import { browserHistory } from 'react-router'
 import makeRootReducer from './reducers'
 import { updateLocation } from './location'
+import createLogger from 'redux-logger'
 
+const logger = createLogger()
 export default (initialState = {}) => {
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk]
+  const middleware = [thunk, logger]
 
   // ======================================================
   // Store Enhancers
